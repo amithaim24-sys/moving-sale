0dfd1aca3053867c08bd4b98bbdcf577dc380e53
Amit <amit@babybloom.co.il>
2026-05-18 23:00:42 +0300

    chore: site title -> 'We move and you can earn from it'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,4 +5,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return children;
 }
 
-export const metadata = { title: "Moving Sale" };
+export const metadata = { title: "We move and you can earn from it" };
